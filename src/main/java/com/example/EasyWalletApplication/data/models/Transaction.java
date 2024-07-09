971d0c68cf3a77b1087c4a74b1c3a92b0f950e12
Ojo Oluwatobi <ojot630@gmail.com>
2024-07-09 13:15:09 +0100

    Added createdAt on 09/07/2024

--- src/main/java/com/example/EasyWalletApplication/data/models/Transaction.java
+++ src/main/java/com/example/EasyWalletApplication/data/models/Transaction.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -20,4 +21,5 @@ public class Transaction {
     private String recipient_name;
     @Enumerated(EnumType.STRING)
     private Status status = Status.PROCESSING;
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
